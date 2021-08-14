19741f5675bf0f903499883a7dd351affa08cbe8
Hirofumi Konno <ancient17.hirofumi@gmail.com>
2021-08-14 18:13:32 +0900

    [Add]scaffold

---
+++ QuickMaster/QuickMaster/Controllers/BooksController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuickMaster.Models;
+
+namespace QuickMaster.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly MyContext _context;
+
+        //コンテキストを準備
+        public BooksController(MyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Books(非同期でデータベースにアクセス)
+        // async:非同期通信
+        public async Task<IActionResult> Index()
+        {
+            //データベースにアクセス＆取得した結果をリスト化
+            //await:一度別スレッドに制御を移したうえで、完了後に再度実行
+            return View(await _context.Book.ToListAsync());
+        }
+
+        // GET: Books/Details/5
+        //リクエストデータidを取得
+        //int?:null許容型
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //引数idをキーにデータベースを検索
+            //FirstOrDefault:検索して最初に見つかったものを返す
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            //データがない場合、404エラー
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Price,Publisher,Sample")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Price,Publisher,Sample")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await _context.Book.FindAsync(id);
+            _context.Book.Remove(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Book.Any(e => e.Id == id);
+        }
+    }
+}
--- QuickMaster/QuickMaster/Models/Book.cs
+++ QuickMaster/QuickMaster/Models/Book.cs
@@ -1,9 +1,15 @@
-﻿namespace QuickMaster.Models {
+﻿using System.ComponentModel;
+
+namespace QuickMaster.Models {
     public class Book {
         public int Id { get; set; }
+        [DisplayName("書名")]
         public string Title { get; set; }
+        [DisplayName("価格")]
         public int Price { get; set; }
+        [DisplayName("出版社")]
         public string Publisher { get; set; }
+        [DisplayName("配布サンプル")]
         public bool Sample { get; set; }
     }
 }
