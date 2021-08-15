b41b7c77147b19280d769013d417dd29320457d2
Hirofumi Konno <ancient17.hirofumi@gmail.com>
2021-08-15 13:56:57 +0900

    [Fix]update

--- QuickMaster/QuickMaster/Controllers/BooksController.cs
+++ QuickMaster/QuickMaster/Controllers/BooksController.cs
@@ -57,70 +57,82 @@ namespace QuickMaster.Controllers
             return View();
         }
 
-        // POST: Books/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        //HTTP POSTで実行されるアクション（つけないとGET時に実行される）
         [HttpPost]
         [ValidateAntiForgeryToken]
+        //ポストデータを引数にバインド（引数としてBook型を指定）
         public async Task<IActionResult> Create([Bind("Id,Title,Price,Publisher,Sample")] Book book)
         {
             if (ModelState.IsValid)
             {
+                //モデルをデータベースに反映
                 _context.Add(book);
+                //反映をセーブする
                 await _context.SaveChangesAsync();
+                //処理後は一覧画面にリダイレクト
                 return RedirectToAction(nameof(Index));
             }
             return View(book);
         }
 
-        // GET: Books/Edit/5
+        // [Edit] リンクから呼び出され、編集フォームを生成
         public async Task<IActionResult> Edit(int? id)
         {
+            //引数が空の場合は404Not Found
             if (id == null)
             {
                 return NotFound();
             }
 
+            //引数idをキーに書籍情報を取得
             var book = await _context.Book.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
             }
+            //編集フォームを表示
             return View(book);
         }
 
-        // POST: Books/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[Save]ボタンで編集内容をデータベースに反映
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Price,Publisher,Sample")] Book book)
         {
+            //隠しフィールドのid値と、URLパラメーターのidが等しいかをチェック
             if (id != book.Id)
             {
                 return NotFound();
             }
 
+            //入力値に問題がなければ更新処理
             if (ModelState.IsValid)
             {
                 try
                 {
+                    //モデルの更新をデータベースに反映
                     _context.Update(book);
                     await _context.SaveChangesAsync();
                 }
+                //競合が発生した場合の処理
                 catch (DbUpdateConcurrencyException)
                 {
+                    //書籍が存在しなければ404
                     if (!BookExists(book.Id))
                     {
                         return NotFound();
                     }
                     else
                     {
+                        //書籍が存在する場合は例外
                         throw;
                     }
                 }
+                //データベース更新に成功した場合はリダイレクト
                 return RedirectToAction(nameof(Index));
             }
+            //入力値に問題がある場合は編集フォームを再描画
             return View(book);
         }
 
@@ -153,6 +165,7 @@ namespace QuickMaster.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //指定された書籍が存在するかを判定
         private bool BookExists(int id)
         {
             return _context.Book.Any(e => e.Id == id);
